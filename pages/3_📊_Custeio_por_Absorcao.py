7bb66c2013377e6694d359e620b8bde14cbf8b82
Flavio Barboza <flmbarboza@gmail.com>
2025-06-24 02:15:48 -0300

    Update 3_📊_Custeio_por_Absorcao.py

--- pages/3_📊_Custeio_por_Absorcao.py
+++ pages/3_📊_Custeio_por_Absorcao.py
@@ -67,15 +67,15 @@ def main():
     dot.node('E', 'Estoque', **node_attr)
     dot.node('CPV', 'Custo dos Produtos Vendidos', **node_attr)
     
-    # Edges que saem de CI — com minlen maior para alongar só essas arestas
-    dot.edge('CI', 'PA', xlabel="Rateio", color="red", fontcolor="red", style='bold', minlen='4')
-    dot.edge('CI', 'PB', xlabel="Rateio", color="red", fontcolor="red", style='bold', minlen='4')
-    dot.edge('CI', 'PC', xlabel="Rateio", color="red", fontcolor="red", style='bold', minlen='4')
-    
     # Edges que saem de CD — também com minlen maior para alongar
     dot.edge('CD', 'PA', color='blue', penwidth='3', arrowhead='vee', style='solid', minlen='3')
     dot.edge('CD', 'PB', color='blue', penwidth='3', arrowhead='vee', style='solid', minlen='3')
     dot.edge('CD', 'PC', color='blue', penwidth='3', arrowhead='vee', style='solid', minlen='3')
+    
+    # Edges que saem de CI — com minlen maior para alongar só essas arestas
+    dot.edge('CI', 'PA', xlabel="Rateio", color="red", fontcolor="red", style='bold', minlen='2.5')
+    dot.edge('CI', 'PB', xlabel="Rateio", color="red", fontcolor="red", style='bold', minlen='2.5')
+    dot.edge('CI', 'PC', xlabel="Rateio", color="red", fontcolor="red", style='bold', minlen='2.5')
     
     # Produtos para Estoque
     dot.edge('PA', 'E')
@@ -89,7 +89,7 @@ def main():
     dot.edge('CPV', 'R')
     
     # Despesas para Resultado
-    dot.edge('D', 'R', minlen='5')
+    dot.edge('D', 'R', minlen='4')
     
     # Vendas para Resultado
     dot.edge('V', 'R')

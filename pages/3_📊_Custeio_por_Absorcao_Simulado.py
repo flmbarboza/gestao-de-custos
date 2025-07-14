2f1114acd0f2693d008aa8c2b7c6913a4bb2d1fd
Flavio Barboza <flmbarboza@gmail.com>
2025-07-14 18:44:40 -0300

    Update 3_📊_Custeio_por_Absorcao_Simulado.py

--- pages/3_📊_Custeio_por_Absorcao_Simulado.py
+++ pages/3_📊_Custeio_por_Absorcao_Simulado.py
@@ -157,8 +157,8 @@ def main():
     with cols[1]:
         if st.button("📊 Avançar para Custeio Variável"):
             st.switch_page("pages/4_📈_Custeio_Variavel.py")
-    with cols[2]:
-        subcol1, subcol2 = st.columns([1,2])  # 3:1 ratio
+    with cols[3]:
+        subcol1, subcol2, subcol3 = st.columns([1,3,1])  # 3:1 ratio
         with subcol2:
             if st.button("🔄 Refazer Teste"):
                 st.session_state.score = 0
